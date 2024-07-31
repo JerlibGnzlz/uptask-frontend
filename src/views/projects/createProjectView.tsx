2a0b93826985f0c879d734e793d7a3286ccb33b7
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-31 13:32:44 -0300

    useForm

--- src/views/projects/createProjectView.tsx
+++ src/views/projects/createProjectView.tsx
@@ -1,19 +1,50 @@
 import { Link } from "react-router-dom";
-
+import { useForm } from 'react-hook-form'
 
 
 export default function CreateProjectView() {
+
+
+    const initialValues = {
+        projectName: "",
+        clientName: "",
+        description: ""
+    }
+
+    const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+    const handleForm = (data) => {
+        console.log(data)
+    }
+
+
     return (
         <>
-            <h1 className="text-5xl font-black">Crear Proyecto</h1>
-            <p className="text-2xl font-light text-gray-500 mt-5 ">Llena el siguiente formulario para crear un proyecto</p>
+            <div className="max-w-3xl mx-auto">
 
-            <nav className="my-5" >
-                <Link to={"/"}
-                    className="bg-purple-400 hover:bg-purple-500 text-white font-bold p-3 text-xl cursor-pointer transition-colors "
+                <h1 className="text-5xl font-black">Crear Proyecto</h1>
+                <p className="text-2xl font-light text-gray-500 mt-5 ">Llena el siguiente formulario para crear un proyecto</p>
+
+                <nav className="my-5" >
+                    <Link to={"/"}
+                        className="bg-purple-400 hover:bg-purple-500 text-white font-bold p-3 text-xl cursor-pointer transition-colors "
+                    >
+                        Volver a proyectos</Link>
+                </nav>
+
+
+                <form
+                    className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+                    onClick={handleSubmit(handleForm)}
+                    noValidate
                 >
-                    Volver a proyectos</Link>
-            </nav>
+                    <input
+                        type="submit"
+                        value={"Crear Proyecto"}
+                        className="bg-fuchsia-600  hover:bg-fuchsia-700 w-full p-3 text-white uppercase  font-bold cursor-pointer transition-colors"
+                    />
+                </form>
+            </div>
         </>
     )
 }
