3bc9d1a27c41bd56c03624195ea287eb3afed7ef
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-05 19:10:45 -0300

    instalar devTools reactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import './index.css'
 import { Router } from './router';
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider >
   </React.StrictMode >
 )
