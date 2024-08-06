80ba490ca817cf550a865164a8ae6c170dd1d379
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-06 18:40:23 -0300

    details

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import AppLayout from "@/layouts/AppLayout";
 import DashBoardView from "@/views/DashBoardView";
 import CreateProjectView from '@/views/projects/createProjectView';
 import EditProjectView from '@/views/projects/editProjectView';
+import DetailProjectView from "./views/projects/detailProject";
 
 
 export const Router = () => {
@@ -13,6 +14,7 @@ export const Router = () => {
                 <Route element={<AppLayout />}>
                     <Route path="/" element={<DashBoardView />} index />
                     <Route path="/projects/create" element={<CreateProjectView />} />
+                    <Route path="/projects/:projectId" element={<DetailProjectView />} />
                     <Route path="/projects/:projectId/edit" element={<EditProjectView />} />
                 </Route>
             </Routes>
---
+++ src/views/projects/detailProject.tsx
@@ -0,0 +1,8 @@
+
+export default function DetailProjectView() {
+    return (
+        <div>
+            <h1>DetailProjectView</h1>
+        </div>
+    )
+}
