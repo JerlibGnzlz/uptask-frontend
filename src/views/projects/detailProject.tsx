ebd5bdceaf9ae962e212c56abdb459e89b9c8cf8
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-06 19:59:26 -0300

    modal

--- src/views/projects/detailProject.tsx
+++ src/views/projects/detailProject.tsx
@@ -1,8 +1,43 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { useQuery } from '@tanstack/react-query'
+import { getProjectById } from "@/api/ProjectAPI"
+import AddTaskModal from "@/components/tasks/AddtaskModal"
+
+
 
 export default function DetailProjectView() {
-    return (
-        <div>
-            <h1>DetailProjectView</h1>
-        </div>
+
+
+    const navigate = useNavigate()
+
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["editProject", projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+    })
+
+    if (isLoading) return "Cargando..."
+    if (isError) return <Navigate to={"/404"} />
+    if (data) return (
+        <>
+            <h1 className="text-5xl font-bold">{data.projectName}</h1>
+            <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+
+            <nav className="my-5 flex gap-3">
+                <button
+                    type="button"
+                    className="bg-purple-400 hover:bg-purple-600 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+                    onClick={() => navigate("?newTask=true")}
+                >
+                    Agregar tarea
+                </button>
+            </nav>
+
+
+            <AddTaskModal />
+        </>
     )
 }
