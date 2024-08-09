18e66724bc22f2b331909436c0357b05bdd2c4a1
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-09 19:56:00 -0300

    diccionario status

--- src/components/tasks/TaslList.tsx
+++ src/components/tasks/TaslList.tsx
@@ -18,16 +18,34 @@ export default function TaskList({ tasks }: TaskListProps) {
         inProgress: [],
         underReview: [],
         complete: []
-
     }
 
-    const groupedTasks = tasks.reduce((acc, task) => {
-        let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
-        currentGroup = [...currentGroup, task]
-        return { ...acc, [task.status]: currentGroup };
-    }, initialStatusGroup);
+    const statusStyles: { [key: string]: string } = {
+        pending: "border-t-slate-500",
+        onHold: "border-t-red-500",
+        inProgress: "border-t-blue-500",
+        underReview: "border-t-amber-500",
+        complete: "border-t-green-500",
+    }
 
-    console.log(groupedTasks)
+    const statusTraslations: { [key: string]: string } = {
+        pending: "Pendiente",
+        onHold: "En espera",
+        inProgress: "En progreso",
+        underReview: "En revision",
+        complete: "Completado"
+    }
+
+
+    const groupedTasks: GroupTask = { ...initialStatusGroup };
+
+    tasks.forEach(task => {
+        if (!groupedTasks[task.status]) {
+            groupedTasks[task.status] = [];
+        }
+        groupedTasks[task.status].push(task);
+    });
+
     return (
         <>
             <h2 className="text-5xl font-black my-10">Tareas</h2>
@@ -35,6 +53,12 @@ export default function TaskList({ tasks }: TaskListProps) {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+
+                        <h3
+                            className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 text-center ${statusStyles[status]}`}>
+                            {statusTraslations[status]}
+                        </h3>
+
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
