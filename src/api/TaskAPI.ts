8b70875301750233f8f2275fca16e6095d2fb4fd
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-13 23:37:00 -0300

    modal de editaskdata

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,10 +1,11 @@
 import { api } from "@/lib/axios"
-import { Project, TaskFormData } from "@/types/index"
+import { Project, Task, TaskFormData } from "@/types/index"
 import { isAxiosError } from "axios"
 
 type TaskAPI = {
     formData: TaskFormData,
     projectId: Project["_id"]
+    taskId: Task["_id"]
 }
 
 export const createTask = async ({ formData, projectId }: Pick<TaskAPI, "formData" | "projectId">) => {
@@ -13,6 +14,21 @@ export const createTask = async ({ formData, projectId }: Pick<TaskAPI, "formDat
         const url = `/projects/${projectId}/tasks`
         const { data } = await api.post<string>(url, formData)
         return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export const getTaskById = async ({ projectId, taskId }: Pick<TaskAPI, "taskId" | "projectId">) => {
+
+    try {
+        const url = `projects/${projectId}/tasks/${taskId}`
+        const { data } = await api(url)
+        return data
+        console.log(data)
+
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
