6a773efddad73b04ca1b1f96aacf5a000fd0d103
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-11-04 23:28:04 -0300

    formulario

--- src/views/auth/NewPassword.tsx
+++ src/views/auth/NewPassword.tsx
@@ -2,10 +2,10 @@
 export default function NewPassword() {
     return (
         <>
-            <h1 className="text-5xl font-black text-white">Crear Cuenta</h1>
+            <h1 className="text-5xl font-black text-white">Restablecer Password</h1>
             <p className="text-2xl font-light text-white mt-5">
-                Llena el formulario para {''}
-                <span className=" text-fuchsia-500 font-bold"> crear tu cuenta</span>
+                Ingresa el codigo que recibistes {''}
+                <span className=" text-fuchsia-500 font-bold"> por Email</span>
             </p>
 
         </>
