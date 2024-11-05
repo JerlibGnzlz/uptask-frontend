cb57e9195b5caa8ba6050640e04c5c49ffcf6a41
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-11-04 23:29:53 -0300

    formulario

--- src/views/auth/NewPassword.tsx
+++ src/views/auth/NewPassword.tsx
@@ -2,10 +2,10 @@
 export default function NewPassword() {
     return (
         <>
-            <h1 className="text-5xl font-black text-white">Restablecer Password</h1>
-            <p className="text-2xl font-light text-white mt-5">
+            <h1 className="text-4xl font-black text-white">Restablecer Password</h1>
+            <p className="text-lg font-light text-white mt-5">
                 Ingresa el codigo que recibistes {''}
-                <span className=" text-fuchsia-500 font-bold"> por Email</span>
+                <span className=" text-fuchsia-500 font-bold"> por email</span>
             </p>
 
         </>
