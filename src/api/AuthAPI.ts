f657414e237fe053b44d71756474d44b5ea0ccc7
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-11-04 23:17:14 -0300

    forgotPassword

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -53,5 +53,17 @@ export const authLogin = async (formData: UserLoginForm) => {
 }
 
 
+export const forgotPassword = async (formData: UserLoginForm) => {
+    try {
+        const url = "/auth/forgotPassword"
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
 
 
--- src/views/auth/NewPassword.tsx
+++ src/views/auth/NewPassword.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 export default function NewPassword() {
     return <div>NewPassword</div>;
